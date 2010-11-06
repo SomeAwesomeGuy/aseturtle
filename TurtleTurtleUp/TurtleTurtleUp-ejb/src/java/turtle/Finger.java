69b0af5c7f67dac0310eae2993276bb773702365
wangsean21@gmail.com <wangsean21@gmail.com@36e224b2-fa50-625f-fa06-99f773100e04>
2010-11-06 05:50:14 +0000

    Added workaround for database management in TurtleLogic

--- TurtleTurtleUp/TurtleTurtleUp-ejb/src/java/turtle/Finger.java
+++ TurtleTurtleUp/TurtleTurtleUp-ejb/src/java/turtle/Finger.java
@@ -10,12 +10,18 @@ package turtle;
  * @author Sean
  */
 public enum Finger {
-    THUMB("thumb"), INDEX("index"), MIDDLE("middle"), RING("ring"), PINKIE("pinky");
+    NONE("none", 0), THUMB("thumb", 1), INDEX("index", 2), MIDDLE("middle", 3), RING("ring", 4), PINKIE("pinky", 5);
 
     private final String name;
+    private final int value;
 
-    private Finger(String name) {
+    private Finger(String name, int value) {
         this.name = name;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
     }
 
     @Override
